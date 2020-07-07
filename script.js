fe4d54b41b7c4b9028f24b593da664c659c90012
EagleEye29 <64352374+EagleEye29@users.noreply.github.com>
2020-07-07 15:48:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var restart = document.querySelector("#b");
+
+var squares = document.querySelectorAll('td');
+
+function clearTable() {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].textContent = '';
+  }
+}
+
+restart.addEventListener('click',clearTable);
+
+for (var i = 0; i < squares.length; i++) {
+  squares[i].addEventListener('click',changeMarker);
+}
+
+function changeMarker() {
+  if(this.textContent === ''){
+    this.textContent = 'X';
+  }
+  else if(this.textContent === 'X'){
+    this.textContent = 'O';
+  }
+  else{
+    this.textContent = '';
+  }
+}
